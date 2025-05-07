7c420293266777d1af448c600563979b0d2ee198
hidemi-kawai <kawai@osask.jp>
2025-05-07 14:28:09 +0900

    Update kuas.c - kuas05a相当に更新

--- kuas/kuas.c
+++ kuas/kuas.c
@@ -1,87 +1,64 @@
-#include "aclmini.c"
+#include "acl3.c"
 
 AClass(Label) { Ai sec, ofs; };
-
+#define cmd(s, t)	(strcmp(s, t) == 0)
 Ai istk0; // 多分将来的に不要になるので、operand0()の引数には積まない.
 
-Auc *skpSpc(Auc *p) { while (0 < *p && *p <= ' ') p++; return p; }
-#define cmd(s, t)	(strcmp(s, t) == 0)
-int opr0Sub(Auc c) { return (c == '+' || c == '-' || c <= ' ' || c == ','); }
-
-Ai operand0(Auc *p, Auc **pp, Ai *pbas, ATokenMgr *tokn, Label *lb)
+Ai operand0(ACStr p, ACStr *pp, Ai *pbas, ATokenMgr *tokn, Label *lb)
 {
-	Ai bas = 0, i = 0, j, sgn;
+	Ai bas = 0, i = 0, l, sgn; ACStr q;
 	while (*p != '\0') {
 		sgn = +1;
-		if (*p == '+') p = skpSpc(p + 1);
-		if (*p == '-') { p = skpSpc(p + 1); sgn = -1; }
-		Auc *q = p;
-		if (bas == 0 && p[0] == 'z' && opr0Sub(p[2])) { bas = 0; p++; }
-		if (bas == 0 && p[0] == 'f' && p[1] == 'p' && opr0Sub(p[2]) && sgn > 0) { bas = 1; p += 2; }
-		if (bas == 0 && p[0] == 'g' && p[1] == 'p' && opr0Sub(p[2]) && sgn > 0) { bas = 2; p += 2; }
-		if (bas == 0 && strncmp((AStr) p, "zero", 4) == 0 && opr0Sub(p[4]))     { bas = 0; p += 4; }
-		if (p > q) { p = skpSpc(p); continue; }
-		j = strtol((AStr) q, (char **) &p, 0); // 定数.
-		if (p > q) { p = skpSpc(p); i += sgn * j; continue; }
-		for (j = 0; !opr0Sub(p[j]); j++); // ラベル参照.
-		if (j == 4 && memcmp(p, "$top", 4) == 0) { i += sgn * istk0; p = skpSpc(p + j); continue; }
-		if (j > 0) {
-			i += sgn * lb[ATokenMgr_s2i(tokn, (AStr) p, j)].ofs;
-			p = skpSpc(p + j);
-			continue;
-		}
-		break;
+		if (*p == '+') p = AskpSpc(p + 1);
+		if (*p == '-') { p = AskpSpc(p + 1); sgn = -1; }
+		l = strtol(p, (char **) &q, 0); if (q > p) { p = AskpSpc(q); i += sgn * l; continue; } // 定数.
+		l = AnamLen1(p, "$"); if (l == 0) break;
+		if (bas == 0 && AnamCmp("z",  p, l))            { bas = 0; p = AskpSpc(p + l); continue; }
+		if (bas == 0 && AnamCmp("fp", p, l) && sgn > 0) { bas = 1; p = AskpSpc(p + l); continue; }
+		if (bas == 0 && AnamCmp("gp", p, l) && sgn > 0) { bas = 2; p = AskpSpc(p + l); continue; }
+		if (bas == 0 && AnamCmp("zero", p, l))          { bas = 0; p = AskpSpc(p + l); continue; }
+		if (AnamCmp("$top", p, l)) { i += sgn * istk0; p = AskpSpc(p + l); continue; }
+		i += sgn * lb[ATokenMgr_s2i(tokn, p, l)].ofs; p = AskpSpc(p + l);
 	}
 	*pbas = bas;
 	if (pp != NULL) *pp = p;
 	return i;
 }
 
-Ai operand1(Auc *p)
+Ai operand1(ACStr p)
 {
-	if (p[0] == 'f' && p[1] == 'p'        && p[2] <= ' ') return 0;
-	if (p[0] == 'g' && p[1] == 'p'        && p[2] <= ' ') return 1;
-	if (strncmp((AStr) p, "isr", 3) == 0  && p[3] <= ' ') return 2;
+	if (p[0] == 'f' && p[1] == 'p' && p[2] <= ' ') return 0;
+	if (p[0] == 'g' && p[1] == 'p' && p[2] <= ' ') return 1;
+	if (strncmp(p, "isr", 3) == 0  && p[3] <= ' ') return 2;
 	return 3;
 }
 
-int uAsmMain(Auc *src, AExpMem *pmem, AExpMem *dmem, ATokenMgr *tokn, AExpMem *labl, char *errMsg)
+int asmMain(ACStr src, AX *pmem, AX *dmem, ATokenMgr *tokn, AX *labl, AStr errMsg)
 {
-	Ai pass, i, im = 0; Auc *p, *q, *src0 = src;
+	Ai pass, i, im = 0; ACStr p, q, src0 = src;
 	Ai pmem0 = pmem->n, dmem0 = dmem->n;
+	AX linBuf[1]; AX_ini(linBuf);
 	for (pass = 0; pass < 2; pass++) {
 		Ai sec = -1, ofs[2], bas, isp = 15;
 		int16_t istk[16]; istk[isp] = 0; istk[(isp + 1) & 15] = 0;
 		pmem->n = pmem0; dmem->n = dmem0;
 		for (src = src0; *src != '\0'; ) {
-			Auc linBuf[1000];
-			while (*src <= ' ' && *src != '\n')
-				src++; // 行頭のスペース類を読み飛ばす.
-			p = (Auc *) strchr((AStr) src, '\n');
-			i = p - src;
-			if (i >= (Ai) sizeof linBuf) {
-				sprintf(errMsg, "too long line (linBufSz=%d)\n", (int) sizeof linBuf);
-				return 1;
-			}
-			memcpy(linBuf, src, i);
-			src += i + 1;
-			while (i > 0 && linBuf[i - 1] <= ' ')
-				i--; // 行末のスペース類を取り除く.
-			linBuf[i] = '\0';
-			p = linBuf;
-			if (*p == '\0') continue; // 空行.
+			src = AX_gets(linBuf, AskpSpc(src));
+			p = (ACStr) linBuf->p; i = linBuf->n;
+			while (i > 0 && p[i - 1] <= ' ') i--; // 行末のスペース類を取り除く.
+			((AStr) p)[i] = '\0'; if (i == 0) continue; // 空行.
 
-			AExpMem_rsv(labl, ASz(Label) * (tokn->n + 9));
-			AExpMem_rsv(pmem, ASz(int32_t) * 9);
-			AExpMem_rsv(dmem, ASz(Auc) * 9999);
+			AX_rsv(labl, Asz(Label) * (labl->n + 9));
+			AX_rsv(pmem, Asz(int32_t) * 9);
+			AX_rsv(dmem, Asz(Auc) * 9999);
 			int32_t *ppm = (int32_t *) (pmem->p + pmem->n);
 			Auc     *pdm = dmem->p + dmem->n;
 			Label   *lb  = (Label *) labl->p;
 			istk0 = istk[isp & 15];
 
-			if ((q = (Auc *) strchr((AStr) p, ':')) != NULL && sec >= 0) {	// def-label.
-				i = ATokenMgr_s2i(tokn, (AStr) p, q - p);
-				ofs[0] = pmem->n / ASz(int32_t);
+			if ((q = strchr(p, ':')) != NULL && sec >= 0) {	// def-label.
+				i = ATokenMgr_s2i(tokn, p, q - p);
+				ofs[0] = pmem->n / Asz(int32_t);
 				ofs[1] = dmem->n;
 				lb[i].sec = sec; lb[i].ofs = ofs[sec]; continue;
 			}
@@ -90,15 +67,15 @@ int uAsmMain(Auc *src, AExpMem *pmem, AExpMem *dmem, ATokenMgr *tokn, AExpMem *l
 			for (i = 0; i < 14 && p[i] > ' '; i++)
 				r[i + 1] = p[i];
 			r[i + 1] = '\0';
-			q = skpSpc(p + i);
+			q = AskpSpc(p + i);
 			r[0] = '0';
 			if (*q != '\0') {
 				r[0] = '2';
-				if (q[0] == 'f' && q[1] == 'p' && q[2] == ',') { q = skpSpc(q + 3); r[0] = '4'; }
+				if (q[0] == 'f' && q[1] == 'p' && q[2] == ',') { q = AskpSpc(q + 3); r[0] = '4'; }
 				if (cmd(r, "2section")) { // 特別なオペランドを持つ命令.
 					sec = -1;
-					if (cmd((AStr) q, ".text")) sec = 0;
-					if (cmd((AStr) q, ".data")) sec = 1;
+					if (cmd(q, ".text")) sec = 0;
+					if (cmd(q, ".data")) sec = 1;
 					continue;
 				}
 				if (cmd(r, "2pop")) { *ppm = 0x1c820 | operand1(q); goto next; }
@@ -112,7 +89,7 @@ int uAsmMain(Auc *src, AExpMem *pmem, AExpMem *dmem, ATokenMgr *tokn, AExpMem *l
 					*pdm++ = im & 0xff;
 					dmem->n++;
 					if (*q != ',') break;
-					q = skpSpc(q + 1);
+					q = AskpSpc(q + 1);
 				}
 				continue;
 			}
@@ -166,41 +143,34 @@ int uAsmMain(Auc *src, AExpMem *pmem, AExpMem *dmem, ATokenMgr *tokn, AExpMem *l
 			if (grp == 0x00) {
 				*ppm = op;
 next:
-				pmem->n += ASz(int32_t);
+				pmem->n += Asz(int32_t);
 				continue;
 			}
 			if (grp == 0x01) { *ppm = op | bas << 12 | (im & 0xfff); goto next; }
 			if (grp == 0x02) { *ppm = op | (im & 0xffff); goto next; } // push
 			if (grp == 0x03) { *ppm = op | ((-im) & 0xf); goto next; } // dup 0 / dup 1
-			if (grp == 0x04) { Ai ip = pmem->n / ASz(int32_t) + 1; *ppm = op | ((im - ip) & 0x3fff); goto next; } // call
-			if (grp == 0x05) { Ai ip = pmem->n / ASz(int32_t) + 1; *ppm = op | ((im - ip) & 0x0fff); goto next; } // jmp / jz
+			if (grp == 0x04) { Ai ip = pmem->n / Asz(int32_t) + 1; *ppm = op | ((im - ip) & 0x3fff); goto next; } // call
+			if (grp == 0x05) { Ai ip = pmem->n / Asz(int32_t) + 1; *ppm = op | ((im - ip) & 0x0fff); goto next; } // jmp / jz
 err:
-			sprintf(errMsg, "mikan(abort): %s\n", p); 
-			return 1;
+			sprintf(errMsg, "mikan(abort): %s\n", p);
+			AX_dst(linBuf); return 1;
 		}
 	}
-	return 0;
+	AX_dst(linBuf); return 0;
 }
 
-#define UseMlcNumSz		0
-
-FILE *myOpen(const char *path, const char *mod)
-{
-	FILE *fp = fopen(path, mod);
-	if (fp == NULL) { fprintf(stderr, "fopen error : %s\n", path); exit(1); }
-	return fp;
-}
+#define UseM_NumSz		0
 
 int main(int argc, const char **argv)
 {
-	#if (UseMlcNumSz != 0)
-		AMlcStdNumSz m0[1];	AMlc_m0v = AMlcStdNumSz_vt; AMlc_m0 = AMlcStdNumSz_inv(m0, AMlc_m0v);
+	#if (UseM_NumSz != 0)
+		AM_StdNumSz m0[1]; AM_m0v = AM_StdNumSz_vt; AM_m0 = AM_StdNumSz_inv(m0, AM_m0v);
 	#else
-		AMlcStd m0[1];	AMlc_m0v = AMlcStd_vt; AMlc_m0 = AMlcStd_inv(m0, AMlc_m0v);
+		AM_Std m0[1]; AM_m0v = AM_Std_vt; AM_m0 = AM_Std_inv(m0, AM_m0v);
 	#endif
 
 	const char *exe = NULL, *inp = NULL, *map = NULL, *dmf = NULL, *pmf = NULL;
-	Ai i, j, pmsz;
+	Ai i, pmsz;
 	for (i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "-o")     == 0) { exe = argv[++i]; continue; }
 		if (strcmp(argv[i], "--map")  == 0) { map = argv[++i]; continue; }
@@ -210,74 +180,45 @@ int main(int argc, const char **argv)
 		fprintf(stderr, "multiple inputs are not supported: '%s'\n", argv[i]);
 		return 1;
 	}
-	AExpMem src[1], pmem[1], dmem[1], labl[1];
-	AExpMem_ini(src); AExpMem_ini(pmem); AExpMem_ini(dmem); AExpMem_ini(labl);
-	FILE *fp = stdin;
-	if (inp != NULL && strcmp(inp, "-") != 0)
-		fp = myOpen(inp, "rb");
-	do {
-		AExpMem_rsv(src, 65536);
-		i = fread(src->p + src->n, 1, 65536, fp);
-		src->n += i;
-	} while (i > 0);
-	if (fp != stdin) fclose(fp);
-	AExpMem_rsv(src, 2);
-	src->p[src->n++] = '\n'; // LFのない行はない.
-	src->p[src->n++] = '\0';
+	AX src[1], pmem[1], dmem[1], labl[1]; AX_ini4(src, pmem, dmem, labl);
+	AX_fread1(src, inp, 1+4);
 
-	char errMsg[256];
-	ATokenMgr tokn[1]; ATokenMgr_ini(tokn);
-	if (exe != NULL) {
-		AExpMem_rsv(dmem, 4);
-		dmem->n = 4;
-	}
-	int errCode = uAsmMain(src->p, pmem, dmem, tokn, labl, errMsg);
+	char errMsg[256]; ATokenMgr tokn[1]; ATokenMgr_ini(tokn);
+	if (exe != NULL) { AX_rsv(dmem, 4); dmem->n = 4; }
+	int errCode = asmMain((ACStr) src->p, pmem, dmem, tokn, labl, errMsg);
 	if (errCode != 0) fprintf(stderr, "%s\n", errMsg);
 	fprintf(stderr, "size of .data: %d bytes\n", (int) dmem->n);
-	fprintf(stderr, "size of .text: %d instructions\n", (int) (pmsz = pmem->n / ASz(int32_t)));
+	fprintf(stderr, "size of .text: %d instructions\n", (int) (pmsz = pmem->n / Asz(int32_t)));
+	int32_t *pi = (int32_t *) pmem->p; Label *lb  = (Label *) labl->p;
 	if (dmf != NULL) {
-		fp = myOpen(dmf, "wt");
+		FILE *fp = AOpen(dmf, "wt");
 		for (i = 0; i < dmem->n; i += 2)
 			fprintf(fp, "%02X%02X\n", dmem->p[i + 1], dmem->p[i]);
 		fclose(fp);
 	}
 	if (pmf != NULL) {
-		fp = myOpen(pmf, "wt");
-		for (i = 0; i < pmem->n; i += ASz(int32_t))
-			fprintf(fp, "%05X\n", *(int32_t *)(pmem->p + i));
+		FILE *fp = AOpen(pmf, "wt");
+		for (i = 0; i < pmsz; i++)
+			fprintf(fp, "%05X\n", pi[i]);
 		fclose(fp);
 	}
 	if (exe != NULL) {
-		dmem->p[0] =  pmsz       & 0xff;
-		dmem->p[1] = (pmsz >> 8) & 0xff;
-		dmem->p[2] =  dmem->n       & 0xff;
-		dmem->p[3] = (dmem->n >> 8) & 0xff;
-		AExpMem_rsv(dmem, 512);
-		memset(dmem->p + dmem->n, 0, 512);
-		fp = myOpen(exe, "wb");
-		fwrite(dmem->p, 1, (dmem->n + 511) & -512, fp);
-		Auc *pc = AMlc_alc(AMlc_m0, AMlc_m0v, pmsz * 3);
-		int32_t *pi = (int32_t *) pmem->p;
-		for (i = j = 0; i < pmsz; i++, j += 3) {
-			pc[j]     =  pi[i]        & 0xff;
-			pc[j + 1] = (pi[i] >>  8) & 0xff;
-			pc[j + 2] = (pi[i] >> 16) & 0xff;
-		}
-		fwrite(pc, 1, j, fp);
-		fclose(fp);
-		AMlc_fre(AMlc_m0, AMlc_m0v, pc, j);
+		AputBytes(dmem->p + 0, pmsz,    0, 8, -1, -1);
+		AputBytes(dmem->p + 2, dmem->n, 0, 8, -1, -1);
+		while ((dmem->n & 511) != 0) AX_putc(dmem, 0x00); // align 512.
+		for (i = 0; i < pmsz; i++)
+			AX_putBtyes(dmem, pi[i], 0, 8, 16, -1);
+		AX_fwrite1(dmem, exe, "wb");
 	}
 	if (map != NULL) {
-		fp = myOpen(map, "wt");
-		Label *lb  = (Label *) labl->p;
+		FILE *fp = AOpen(map, "wt");
 		for (i = 0; i < tokn->n; i++)
 			fprintf(fp, "#%04d sec=%d ofs=0x%04x : %s\n", (int) i, (int) lb[i].sec, (int) lb[i].ofs, ATokenMgr_i2s(tokn, i));
 		fclose(fp);
 	}
-	ATokenMgr_dst(tokn);
-	AExpMem_dst(src); AExpMem_dst(pmem); AExpMem_dst(dmem); AExpMem_dst(labl);
+	ATokenMgr_dst(tokn); AX_dst4(src, pmem, dmem, labl);
 
-	#if (UseMlcNumSz != 0)
+	#if (UseM_NumSz != 0)
 		fprintf(stderr, "m0.n=%d, m0.s=%d\n", m0->n, m0->s);
 	#endif
 
