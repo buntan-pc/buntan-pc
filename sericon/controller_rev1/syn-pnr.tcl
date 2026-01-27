b2c7a15773c3ca2908b5506fd61e1afdbb71b886
uchan-nos <uchan@uchan.net>
2026-01-28 04:20:00 +0900

    固定のDVI信号を出す（WIP）
    現状、ディスプレイには何も映らない

--- sericon/controller_rev1/syn-pnr.tcl
+++ sericon/controller_rev1/syn-pnr.tcl
@@ -4,6 +4,7 @@ set_option -verilog_std sysv2017
 set_option -print_all_synthesis_warning 1
 set_option -top_module "controller_top"
 set_option -output_base_name "controller"
+set_option -use_sspi_as_gpio 1
 
 add_file "src/port.cst"
 add_file "src/timing.sdc"
