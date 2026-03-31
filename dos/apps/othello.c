037c5983aadd3f7b69a7ef08896bbc123742d027
uchan-nos <uchan@uchan.net>
2026-03-31 13:31:03 +0900

    エスケープシーケンスを使ってオセロ盤面を見やすく表示

--- dos/apps/othello.c
+++ dos/apps/othello.c
@@ -5,9 +5,17 @@ int cx;
 int cy;
 int turn = 1; // 1=black 2=white
 int ai_turn = 2;
+int ai_lastx;
+int ai_lasty;
 
 void print_board_init() {
-  sys_put_string("turn: ", -1);
+  sys_put_string("\x1B[H", -1);  // カーソルを左上へ
+  sys_put_string("ai_turn: ", -1);
+  sys_put_string("*o" + (ai_turn - 1), 1);
+  sys_put_string("\n", 1);
+  sys_put_string("O/@: AI's last move\n\n", -1);
+
+  sys_put_string("current turn: ", -1);
   sys_put_string("*o" + (turn - 1), 1);
   sys_put_string("\n", 1);
 
@@ -27,9 +35,9 @@ void print_board_init() {
 }
 
 void print_board() {
-  sys_put_string("\x1B[1;7H", -1);  // カーソルを turn: の次へ
+  sys_put_string("\x1B[4;15H", -1);  // カーソルを turn: の次へ
   sys_put_string("*o" + (turn - 1), 1);
-  sys_put_string("\x1B[2;1H", -1); // カーソルを 1 行下に
+  sys_put_string("\n", 1);
 
   for (int y = 0; y < 8; ++y) {
     unsigned int line = board[y];
@@ -39,7 +47,17 @@ void print_board() {
       } else {
         sys_put_string(" ", 1);
       }
-      sys_put_string("_*o?" + (line & 3), 1);
+      int st;
+      if (ai_lasty == y && ai_lastx == x) {
+        if (ai_turn == 1) {
+          st = '@';
+        } else {
+          st = 'O';
+        }
+      } else {
+        st = "_*o?"[line & 3];
+      }
+      sys_put_string(&st, 1);
       line = line >> 2;
     }
     sys_put_string("\n", 1);
@@ -50,7 +68,7 @@ int get_stone(unsigned int *board, int x, int y) {
   return (board[y] >> 2*x) & 3;
 }
 
-int try_put_stone(unsigned int *board, int cx, int cy) {
+int try_put_stone(unsigned int *board, int cx, int cy, int stone) {
   if (get_stone(board, cx, cy) != 0) {
     return -1;
   }
@@ -86,14 +104,14 @@ int try_put_stone(unsigned int *board, int cx, int cy) {
       int st = get_stone(board, x, y);
       if (st == 0) {
         break;
-      } else if (st == turn) {
-        if (prev_st == 3 - turn) {
+      } else if (st == stone) {
+        if (prev_st == 3 - stone) {
           rev_cnt += i;
         }
         int putx = x - dx;
         int puty = y - dy;
         for (int j = 0; j < i; ++j) {
-          board[puty] = (board[puty] & ~(3 << 2*putx)) | (turn << 2*putx);
+          board[puty] = (board[puty] & ~(3 << 2*putx)) | (stone << 2*putx);
           putx = putx - dx;
           puty = puty - dy;
         }
@@ -135,7 +153,7 @@ int buntan_main(int *info) {
           for (int i = 0; i < 8; ++i) {
             board_ai[i] = board[i];
           }
-          int rev_cnt = try_put_stone(board_ai, x, y);
+          int rev_cnt = try_put_stone(board_ai, x, y, turn);
           if (max_rev_cnt < rev_cnt) {
             max_rev_cnt = rev_cnt;
             max_x = x;
@@ -144,14 +162,20 @@ int buntan_main(int *info) {
         }
       }
 
-      try_put_stone(board, max_x, max_y);
-      board[max_y] |= ai_turn << 2*max_x;
+      ai_lastx = max_x;
+      ai_lasty = max_y;
+
+      try_put_stone(board, ai_lastx, ai_lasty, turn);
+      board[ai_lasty] |= ai_turn << 2*ai_lastx;
       turn = 3 - turn;
     } else {
       int c = sys_getc();
       if (c <= 0) {
         break;
       }
+
+      // カーソルから右側を消去（ステータスラインを消去）
+      sys_put_string("\x1B[K", -1);
 
       if (cx > 0 && (c == 'h' || c == 0x1F)) {
         --cx;
@@ -166,13 +190,17 @@ int buntan_main(int *info) {
           sys_put_string("cannot put a stone\n", -1);
           continue;
         } else {
-          if (try_put_stone(board, cx, cy) > 0) {
+          if (try_put_stone(board, cx, cy, turn) > 0) {
             board[cy] |= turn << (2*cx);
             turn = 3 - turn;
           } else {
             sys_put_string("cannot put a stone\n", -1);
           }
         }
+      } else if (c == 'q') {
+        sys_put_string("Quitting. Please press a key...\n", -1);
+        sys_getc();
+        break;
       } else {
         continue;
       }
