cdb303932690be3b8e7a2a51158babeff16e2ead
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-05-23 15:41:38 +0900

    cpu.hにCPU初期化、リセット、ステップ、破棄関数の宣言のプロトタイプを追加

--- emulator/cpu.h
+++ emulator/cpu.h
@@ -5,4 +5,17 @@
 #ifndef BUNTAN_EMU_CPU_H
 #define BUNTAN_EMU_CPU_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void cpu_init(void);
+void cpu_reset(void);
+void cpu_step(void);
+void cpu_destroy(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 #endif /* BUNTAN_EMU_CPU_H */
