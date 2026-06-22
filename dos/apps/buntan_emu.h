b2dee08586d1fa804eaebd4a0c144b2af585b7f0
uchan-nos <uchan@uchan.net>
2026-06-22 14:55:40 +0900

    othello.cを無改造でLinux向けにビルドできるようにする

---
+++ dos/apps/buntan_emu.h
@@ -0,0 +1,10 @@
+unsigned int __builtin_get_sr(unsigned int sr_num) {
+  return 0;
+}
+
+unsigned int __builtin_set_sr(unsigned int sr_num, unsigned int value) {
+  return 0;
+}
+
+void __builtin_reset_sr(unsigned int sr_num) {
+}
