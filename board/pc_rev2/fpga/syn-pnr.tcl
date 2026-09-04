88de54ed02b17822193758a1242436459e572d30
uchan-nos <uchan@uchan.net>
2026-09-05 06:46:31 +0900

    rename pc_rev1 -> pc_rev2

--- board/pc_rev2/fpga/syn-pnr.tcl
+++ board/pc_rev2/fpga/syn-pnr.tcl
@@ -3,10 +3,10 @@ set_device -name "GW1NR-9C" "GW1NR-LV9QN88PC6/I5"
 set_option -verilog_std sysv2017
 set_option -print_all_synthesis_warning 1
 set_option -top_module "main"
-set_option -output_base_name "pc_rev1"
+set_option -output_base_name "pc_rev2"
 set_option -use_sspi_as_gpio 1
 
-set flist [open src/pc_rev1.f r]
+set flist [open src/pc_rev2.f r]
 while {[gets $flist line] >= 0} {
   add_file $line
 }
