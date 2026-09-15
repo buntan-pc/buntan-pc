e488fa90abdedd23fc10bedfd65b64bfb46013cb
uchan-nos <uchan@uchan.net>
2026-09-15 14:39:26 +0900

    ADC Rev.2を2us周期に高速化

--- cpu/adc_rev2.sv
+++ cpu/adc_rev2.sv
@@ -16,13 +16,11 @@ module adc_rev2#(
   output logic [7:0] adc_result // ADC の変換結果
 );
 
-localparam PWM_PERIOD = 255;
-localparam TICK_SAMPLE_END = 100;
-localparam TICK_CONVERT_BIT = 100;
+localparam TICK_SAMPLE_END = 54; // 54 clocks = 2.0us
+localparam TICK_CONVERT_BIT = 54; // 54 clocks = 2.0us
 
-logic [7:0] pwm_period, pwm_period_next;
 logic [7:0] adc_sar;         // 逐次比較レジスタ
-logic [8:0] tick, tick_next; // PWM が周回するたびにインクリメントされる
+logic [8:0] tick, tick_next; // クロックを数えるレジスタ
 logic tick_edge_zero;        // tick が次に 0 になる瞬間に 1 になる
 
 typedef enum logic {
@@ -32,22 +30,14 @@ typedef enum logic {
 adc_phase_t adc_phase;
 logic [2:0] bit_index;  // 変換中のビット位置
 
-assign pwm_period_next = pwm_period >= PWM_PERIOD - 1 ? 0 : pwm_period + 1;
 assign tick_next =
   adc_phase == SAMPLE
     ? (tick >= TICK_SAMPLE_END - 1 ? 0 : tick + 1)
     : (tick >= TICK_CONVERT_BIT - 1 ? 0 : tick + 1);
-assign tick_edge_zero = pwm_period_next == 0 & tick_next == 0;
-assign adc_sh_on = ~(adc_phase == SAMPLE);
-assign adc_sel = 3'd0;
+assign tick_edge_zero = tick_next == 0;
+assign adc_sh_on = adc_phase == SAMPLE;
+assign adc_sel = 3'd1;
 assign adc_vref = adc_sar;
-
-always @(posedge rst, posedge clk125) begin
-  if (rst)
-    pwm_period <= 0;
-  else
-    pwm_period <= pwm_period_next;
-end
 
 always @(posedge rst, posedge clk125) begin
   if (rst)
@@ -57,14 +47,14 @@ always @(posedge rst, posedge clk125) begin
     if (bit_index > 0)
       adc_sar[bit_index - 1] <= 1;
   end
-  else if (adc_phase == SAMPLE & tick == 0 & pwm_period_next == 0)
+  else if (adc_phase == SAMPLE & tick == 0)
     adc_sar <= 8'h80;
 end
 
 always @(posedge rst, posedge clk125) begin
   if (rst)
     tick <= 0;
-  else if (pwm_period_next == 0)
+  else
     tick <= tick_next;
 end
 
@@ -88,7 +78,7 @@ end
 always @(posedge rst, posedge clk125) begin
   if (rst)
     adc_result <= 0;
-  else if (adc_phase == SAMPLE & tick == 0 & pwm_period_next == 0)
+  else if (adc_phase == SAMPLE & tick == 0)
     adc_result <= adc_sar;
 end
 
