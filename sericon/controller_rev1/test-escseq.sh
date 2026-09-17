d6f3109de445f2fc17ee2cafdb664af3e914aada
uchan-nos <uchan@uchan.net>
2026-09-17 16:35:16 +0900

    DECRST（代替バッファから標準バッファに戻す）に対応

--- sericon/controller_rev1/test-escseq.sh
+++ sericon/controller_rev1/test-escseq.sh
@@ -3,27 +3,45 @@
 script_dir=$(dirname $0)
 scripts_dir=$script_dir/../../scripts
 uartpy=$scripts_dir/uart.py
+sendhex="$uartpy --baudrate 9600 --nowait"
 
 # CSI 2 r  スクロール範囲
-$uartpy --baudrate 9600 --nowait 1B 5B 32 72
+$sendhex 1B 5B 32 72
 
 # CSI 999 H  カーソルを最下行に
-$uartpy --baudrate 9600 --nowait 1B 5B 39 39 39 48
+$sendhex 1B 5B 39 39 39 48
 
 # DEAD
-$uartpy --baudrate 9600 --nowait 44 45 41 44
+$sendhex 44 45 41 44
 
 # ESC 7  カーソル位置を保存 DECSC
-$uartpy --baudrate 9600 --nowait 1B 37
+$sendhex 1B 37
 
 # CSI 1;1 H  カーソルを左上に
-$uartpy --baudrate 9600 --nowait 1B 5B 31 3B 31 48
+$sendhex 1B 5B 31 3B 31 48
 
 # @^
-$uartpy --baudrate 9600 --nowait 40 5E
+$sendhex 40 5E
 
 # ESC 7  カーソル位置を復帰 DECRC
-$uartpy --baudrate 9600 --nowait 1B 38
+$sendhex 1B 38
 
 # LF BEEF
-$uartpy --baudrate 9600 --nowait 0A 42 45 45 46
+$sendhex 0A 42 45 45 46
+
+# CSI ? 1049 h  DECSET XT_EXTSCRN 代替バッファに切り替え
+$sendhex 1B 5B 3F 31 30 34 39 68
+
+# CSI 1;1 H
+$sendhex 1B 5B 31 3B 31 48
+
+# CAFE LF BABE
+$sendhex 43 41 46 45 0A 42 41 42 45
+
+sleep 2
+
+# CSI ? 1049 l  DECRST XT_EXTSCRN 標準バッファに切り替え
+$sendhex 1B 5B 3F 31 30 34 39 6C
+
+# LF std buffer
+$sendhex 0A 73 74 64 20 62 75 66 66 65 72
